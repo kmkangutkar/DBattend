c0c34d48c205b74337fd12b949d914a5e607a4e5
kmkangutkar <kmkangutkar@users.noreply.github.com>
2017-02-19 18:19:30 +0530

    created db and classes

--- app/src/main/java/com/example/komal/dbattend/DatabaseHelper.java
+++ app/src/main/java/com/example/komal/dbattend/DatabaseHelper.java
@@ -9,16 +9,20 @@ import android.widget.Toast;
 /**
  * Created by komal on 16/2/17.
  */
-
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DBName = "Records";
     private static final String teacherCourseTable = "TeacherCourses";
     private static final String  studentCourseTable = "StudentCourses";
     private static final String attendanceTable = "Attendance";
-    String columnCourseName = "courseName";
-    String columnCourseID = "courseID";
-    String columnCourseType = "courseType";
-    String columnDivID = "divisionID";
+    String columnTCourseName = "courseName";
+    String columnTCourseID = "courseID";
+    String columnTCourseType = "courseType";
+    String columnTDivID = "divisionID";
+    String columnSCourseID = "s_courseID";
+    String columnStudentID = "studentID";
+    String columnStatus = "status";
+    String columnDateTime = "datetime";
+
 
     private static final int DBVersion = 1;
 
@@ -29,35 +33,83 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTable = "CREATE TABLE " + teacherCourseTable +
-                " (" + columnCourseID + " VARCHAR(16), "
-                + columnCourseName + " VARCHAR(32), "
-                + columnCourseType + " VARCHAR(32), "
-                + columnDivID + " INT, PRIMARY KEY (" +columnCourseID + "));";
-        db.execSQL(createTable);
+        String createTeacherCourses = "CREATE TABLE " + teacherCourseTable +
+                " (" + columnTCourseID + " VARCHAR(16), "
+                + columnTCourseName + " VARCHAR(32), "
+                + columnTCourseType + " VARCHAR(32), "
+                + columnTDivID + " INT, PRIMARY KEY (" + columnTCourseID + "));";
+        String createStudentCourses = "CREATE TABLE " + studentCourseTable +
+                " (" + columnSCourseID + " VARCHAR(16), "
+                + columnStudentID + " VARCHAR(16), PRIMARY KEY (" + columnSCourseID + ", " + columnStudentID + ")," +
+                " FOREIGN KEY ( "+ columnSCourseID +") REFERENCES " + teacherCourseTable + " ( " + columnTCourseID +") " +
+                "ON DELETE CASCADE);";
+        String createAttendance = "CREATE TABLE " + attendanceTable +
+                " (" + columnTCourseID + " VARCHAR(16), "
+                + columnStudentID + " VARCHAR(16), "
+                + columnStatus + " VARCHAR(4), "
+                + columnDateTime + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, " +
+                "PRIMARY KEY (" + columnTCourseID + ", " + columnStudentID + ", " + columnStatus + ", " + columnDateTime + "));";
+        try {
+            db.execSQL(createTeacherCourses);
+            db.execSQL(createStudentCourses);
+            db.execSQL(createAttendance);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         String sql = "DROP TABLE IF EXISTS '" + teacherCourseTable +"'";
+        String sql1 = "DROP TABLE IF EXISTS '" + studentCourseTable +"'";
+        String sql2 = "DROP TABLE IF EXISTS '" + attendanceTable +"'";
         db.execSQL(sql);
+        db.execSQL(sql1);
+        db.execSQL(sql2);
         onCreate(db);
     }
 
-
-    public boolean insertData(String id, String name, String type, int div){
+    public boolean insertTeacherCourse(MainActivity.TeacherCourse tc){
         String sql = "INSERT INTO " + teacherCourseTable + " VALUES " +
-                "( '" + id + "', '" + name + "', '" + type + "', '" + div + "')";
+                "( '" + tc.ID + "', '" + tc.name + "', '" + tc.type + "', '" + tc.div + "')";
         SQLiteDatabase db = this.getWritableDatabase();
         db.execSQL(sql);
         db.close();
         return true;
     }
-    public Cursor showData(){
+
+    public boolean insertStudentCourse(MainActivity.StudentCourse sc){
+        String sql = "INSERT INTO " + studentCourseTable + " VALUES " +
+                "( '" + sc.studentID + "', '" + sc.courseID + "')";
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL(sql);
+        db.close();
+        return true;
+    }
+
+    public boolean insertAttendance(MainActivity.Attendance at){
+        String sql = "INSERT INTO " + attendanceTable + "( " + columnTCourseID +", " + columnStudentID + ", "+ columnStatus +") VALUES " +
+                "( '" + at.courseID + "', '" + at.studentID + "', '" + at.status + "')";
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL(sql);
+        db.close();
+        return true;
+    }
+
+    public Cursor getTeacherCoursesData(){
         SQLiteDatabase db = this.getReadableDatabase();
         String sql = "SELECT * FROM " +teacherCourseTable + "";
         Cursor c = db.rawQuery(sql, null);
         return c;
-     }
+    }
+    public Cursor getStudentCourses(String sid){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String sql = "SELECT * FROM " +studentCourseTable +" WHERE " + columnStudentID+"="+sid+"";
+        Cursor c = db.rawQuery(sql, null);
+        return c;
+    }
+
+
+
 }
 
--- app/src/main/java/com/example/komal/dbattend/MainActivity.java
+++ app/src/main/java/com/example/komal/dbattend/MainActivity.java
@@ -4,27 +4,85 @@ import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper db;
-    TextView text;
+    TextView text1, text2;
     String result;
+    int i;
+    Cursor c;
+    StringBuilder stringBuilder = new StringBuilder();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text = (TextView)findViewById(R.id.text);
+        text1 = (TextView)findViewById(R.id.text1);
+        text2 = (TextView)findViewById(R.id.text2);
         db = new DatabaseHelper(this);
 
-        db.insertData("45", "FCP", "programming", 2);
-        Cursor c = db.showData();
-        if(c != null && c.moveToNext()){
-            int i = c.getColumnIndex("courseName");
-            String name = c.getString(i);
-           text.setText(name);
+        //testing start
+        TeacherCourse fcp = new TeacherCourse("65", "FCP", "programming", 2);
+        TeacherCourse cpp = new TeacherCourse("34", "BEE", "electrical", 1);
+        StudentCourse s1 = new StudentCourse("1232", "34");
+        StudentCourse s2 = new StudentCourse("1232", "65");
+        Attendance a1 = new Attendance("1232", "34", "P");
+        db.insertTeacherCourse(fcp);
+        db.insertTeacherCourse(cpp);
+        db.insertStudentCourse(s1);
+        db.insertStudentCourse(s2);
+        db.insertAttendance(a1);
+        c = db.getTeacherCoursesData();
+        while(c != null && c.moveToNext()){
+            i = c.getColumnIndex("courseName");
+            stringBuilder.append(c.getString(i));
+            i = c.getColumnIndex("courseID");
+            stringBuilder.append(c.getString(i));
+            i = c.getColumnIndex("courseType");
+            stringBuilder.append(c.getString(i));
+            i = c.getColumnIndex("divisionID");
+            stringBuilder.append(c.getString(i));
+            String name = stringBuilder.toString();
+            text1.setText(name);
         }
+        c = db.getStudentCourses("1232");
+        if(c != null && c.moveToNext()){
+            i = c.getColumnIndex("s_courseID");
+            String cid = c.getString(i);
+            text2.setText(cid);
+        }else{
+            Toast.makeText(this, "EMPTY", Toast.LENGTH_SHORT).show();
+        }
+        //testing end
 
     }
+    class TeacherCourse{
+        String name, ID, type;
+        int div;
+        TeacherCourse(String id, String name, String type, int div){
+            this.name = name;
+            this.div = div;
+            this.ID = id;
+            this.type = type;
+        }
+    }
+    class StudentCourse{
+        String studentID, courseID;
+        StudentCourse(String sid, String cid){
+            this.studentID = sid;
+            this.courseID = cid;
+        }
+    }
+    class Attendance{
+        String studentID, courseID, status;
+        Attendance(String sid, String cid, String stat){
+            this.studentID = sid;
+            this.courseID = cid;
+            this.status = stat;
+        }
+    }
+
+
 }
